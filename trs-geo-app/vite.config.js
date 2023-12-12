8c37d22d08cf0a32b2a0fd3209b127abcb1fc6f2
jandrusak <jonathanandrusak@gmail.com>
2023-12-12 15:11:40 -0600

    remvoing emotion

--- trs-geo-app/vite.config.js
+++ trs-geo-app/vite.config.js
@@ -2,15 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: "emotion/react", 
-      babel: {
-        plugins: ["@emotion/babel-plugin"], 
-      }, 
-    }), 
-  ],
-  
+  plugins: [react()],
+    // react({jsxImportSource: "emotion/react", babel: {plugins: ["@emotion/babel-plugin"],},}),],
   server: {
     proxy: {
       '/api': {
