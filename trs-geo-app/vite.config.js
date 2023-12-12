675d0b6ff91689279d65561df7b71071bbc30453
jandrusak <jonathanandrusak@gmail.com>
2023-12-12 15:20:47 -0600

    installing and adding in babel

--- trs-geo-app/vite.config.js
+++ trs-geo-app/vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-    // react({jsxImportSource: "emotion/react", babel: {plugins: ["@emotion/babel-plugin"],},}),],
+  plugins: [
+    // react()],
+    react({jsxImportSource: "emotion/react", babel: {plugins: ["@emotion/babel-plugin"],},}),],
   server: {
     proxy: {
       '/api': {
