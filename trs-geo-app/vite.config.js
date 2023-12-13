2c6a887276c8e4b82ec2bb8d936797458e411ad7
jandrusak <jonathanandrusak@gmail.com>
2023-12-12 19:03:33 -0600

    editing for emotion

--- trs-geo-app/vite.config.js
+++ trs-geo-app/vite.config.js
@@ -3,16 +3,33 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // react()],
-    react({jsxImportSource: "emotion/react"})],
-    // , babel: {plugins: ["@emotion/babel-plugin"],},}),],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3009', 
-        changeOrigin: true, 
-        rewrite: (path) => path.replace(/^\/api/ , '')
-      }, 
-    }, 
+    react({
+    jsxImportSource: "emotion/react", babel: {plugins: ["@emotion/babel-plugin"],
   },
 })
+], 
+    resolve: {
+      alias: {
+        'emotion/react/jsx-runtime': '@emotion/react/jsx-runtime',
+      }
+    },
+  build: {
+    rollupOptions: {
+      external: ["emotion/react/jsx-runtime"]
+    }
+  }
+  // [react({
+
+
+  
+  
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3009', 
+  //       changeOrigin: true, 
+  //       rewrite: (path) => path.replace(/^\/api/ , '')
+  //     }, 
+  //   }, 
+  // },
+});
