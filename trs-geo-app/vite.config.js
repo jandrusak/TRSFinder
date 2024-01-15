8dc93c5486e1ac04529595425a01bfe4f45336cf
jandrusak <jonathanandrusak@gmail.com>
2024-01-15 14:23:42 -0600

    removing emotion

--- trs-geo-app/vite.config.js
+++ trs-geo-app/vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: "emotion/react", 
-      babel: {
-        plugins: ["@emotion/babel-plugin"],
-      },
+      // jsxImportSource: "emotion/react",
     }),
   ],
 });
