1fba84a51c04fafe6de6fd083889947ecdff7971
jandrusak <jonathanandrusak@gmail.com>
2024-01-22 14:16:06 -0600

    edit index.js

--- trs-geo-backend/src/index.js
+++ trs-geo-backend/src/index.js
@@ -1,43 +1,27 @@
 const express = require('express')
 const app = express();
 let cors = require("cors");
-require("dotenv").config();
 
-const corsOptions = {
-    origin: ['http://localhost:5173', 'https://trsfinder-frontend.onrender.com'], 
-    optionsSuccessStatus: 200,
-};
+app.use(cors)
 
+require("dotenv").config
 
-app.use(cors(corsOptions));
 app.use(express.json());
-
-//importing routes
 let routes = require("./routes");
-app.use(routes);
-
-let authRoutes = require("./authRoutes");
-app.use(authRoutes);
-
-//database connection
 const db = require('./db');
 
-//Test SQL query 
-let sql = "select now()";
-db.query(sql, function (err, results){
-    //what to do when query results come back? 
-    if(err) {
-        console.error("Running the query failed.", err.message);
-    }   else {
-        console.log("The query was successful. => \n", results);
-    }
-});
-
-//start server
+app.use(routes);
+//database connection
 let PORT = process.env.PORT || 3306;
 app.listen(PORT, function(){
     console.log("GeoApp started on port", PORT);
 });
 
+let authRoutes = require("./authRoutes");
+app.use(authRoutes);
+//start server
+
+let helloRoutes = require("./routes")
+
 //test endpoint 
-app.get('/', (req, res) => res.send('Hello World'));
+console.log("hello world")
