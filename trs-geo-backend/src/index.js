d6b9e4c3f22d1791ca49c4982316c4a55c840340
jandrusak <jonathanandrusak@gmail.com>
2024-01-22 14:26:54 -0600

    edit

--- trs-geo-backend/src/index.js
+++ trs-geo-backend/src/index.js
@@ -2,26 +2,72 @@ const express = require('express')
 const app = express();
 let cors = require("cors");
 
-app.use(cors)
-
 require("dotenv").config
 
+app.use(cors)
 app.use(express.json());
+
 let routes = require("./routes");
-const db = require('./db');
+let authRoutes = require("./authRoutes");
 
 app.use(routes);
-//database connection
+app.use(authRoutes);
+
+app.get('/hello', (req, res) => {
+    res.send("Hello World");
+});
+
 let PORT = process.env.PORT || 3306;
 app.listen(PORT, function(){
     console.log("GeoApp started on port", PORT);
 });
 
-let authRoutes = require("./authRoutes");
-app.use(authRoutes);
-//start server
 
-let helloRoutes = require("./routes")
 
-//test endpoint 
-console.log("hello world")
+
+
+
+
+// const express = require('express')
+// const app = express();
+// let cors = require("cors");
+// require("dotenv").config();
+
+// const corsOptions = {
+//     origin: ['http://localhost:5173', 'https://trsfinder-frontend.onrender.com'], 
+//     optionsSuccessStatus: 200,
+// };
+
+
+// app.use(cors(corsOptions));
+// app.use(express.json());
+
+// //importing routes
+// let routes = require("./routes");
+// app.use(routes);
+
+// let authRoutes = require("./authRoutes");
+// app.use(authRoutes);
+
+// //database connection
+// const db = require('./db');
+
+// //Test SQL query 
+// let sql = "select now()";
+// db.query(sql, function (err, results){
+//     //what to do when query results come back? 
+//     if(err) {
+//         console.error("Running the query failed.", err.message);
+//     }   else {
+//         console.log("The query was successful. => \n", results);
+//     }
+// });
+
+// //start server
+// let PORT = process.env.PORT || 3306;
+// app.listen(PORT, function(){
+//     console.log("GeoApp started on port", PORT);
+// });
+
+// //test endpoint 
+// app.get('/', (req, res) => res.send('Hello World'));
