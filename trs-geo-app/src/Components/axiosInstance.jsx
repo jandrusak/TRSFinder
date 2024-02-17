da544b19862c03e013506bec0f633217538f108a
jandrusak <jonathanandrusak@gmail.com>
2024-02-17 12:36:20 -0600

    edit axios instance

--- trs-geo-app/src/Components/axiosInstance.jsx
+++ trs-geo-app/src/Components/axiosInstance.jsx
@@ -1,10 +1,10 @@
 // axiosInstance.js
 import axios from 'axios';
 
-export const axiosInstance = axios.create();
-
-axiosInstance.interceptors.request.use(function (config) {
-    const token = localStorage.getItem('token');
-    config.headers.Authorization = token ? `Bearer ${token}` : '';
-    return config;
-});
+axios.get('https://trsfinder-backend.onrender.com/Products')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error('There was an error!', error);
+  });
