acc89ce2e57a209ed87587a718d2c52218797726
jandrusak <jonathanandrusak@gmail.com>
2024-02-17 13:36:38 -0600

    add

--- trs-geo-app/src/Components/axiosInstance.jsx
+++ trs-geo-app/src/Components/axiosInstance.jsx
@@ -1,10 +1,10 @@
-// axiosInstance.js
-import axios from 'axios';
+// // axiosInstance.js
+// import axios from 'axios';
 
-axios.get('https://trsfinder-backend.onrender.com/Products')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('There was an error!', error);
-  });
+// axios.get('https://trsfinder-backend.onrender.com/Products')
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(error => {
+//     console.error('There was an error!', error);
+//   });
